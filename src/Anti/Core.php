94af38afd659b8113284e6c81d7cc61f6d7434fd
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-01-26 22:33:15 +0000

    Add files via upload

---
+++ src/Anti/Core.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+namespace Anti;
+use Anti\AntiAdvertising;
+use Anti\AntiSwearing;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as C;
+class Core extends PluginBase {
+    const PERM_RANK = C::BOLD . C::BLUE . "Permission> " . C::RESET . C::GRAY . "You Need A Rank To Access This Command";
+    const PERM_STAFF = C::BOLD . C::BLUE . "Permission> " . C::RESET . C::GRAY . "Only Staff Can Use This Command";
+    const USE_IN_GAME = C::BOLD . C::BLUE . "Command> " . C::RESET . C::GRAY . "Use Command In Game";
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents(($this->antiadvertising = new AntiAdvertising($this)), $this);
+        $this->getServer()->getPluginManager()->registerEvents(($this->antiswearing = new AntiSwearing($this)), $this);
+    }
+}
